81109033899ea89b498ed4affd1d0fe2dd7e8ba2
Benito Rodríguez Arcos <brarcos@gmail.com>
2012-11-30 17:22:23 +0100

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -32,23 +32,7 @@ def exists_file(filename):
     return os.path.exists(filename)
 
 
-def static():
-    # scripts
-    local('wget -qO _static/js/jquery.js http://code.jquery.com/jquery.js')
-    local('wget -qO _static/js/jquery.min.js http://code.jquery.com/jquery.min.js')
-    local('wget -qO _static/js/coffee-script.min.js http://coffeescript.org/extras/coffee-script.js')
-    local('wget -qO _static/js/less.min.js http://lesscss.googlecode.com/files/less-1.3.0.min.js')
-
-    # bootstrap
-    local('rm -rf _static/bootstrap')
-    local('git clone https://github.com/twitter/bootstrap.git _static/bootstrap')
-    local('rm -rf _static/bootstrap/.git')
-    local('make -C _static/bootstrap bootstrap')
-    local('cp _static/bootstrap/bootstrap/js/bootstrap.min.js _static/js')
-
-
 def syncdb():
-    local('chmod +x manage.py')
     local('./manage.py syncdb')
     local('./manage.py migrate')
 
@@ -106,6 +90,9 @@ def install():
 
     # requeriments
     execute(requirements)
+    
+    # others
+    local("chmod +x manage.py")
 
 
 @task
